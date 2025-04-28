32777a1f5bfcc615f7a61e7bd21b8e8a516ece73
Sef <sef.w@outlook.fr>
2025-04-28 15:10:32 +0200

    OK

--- src/App.jsx
+++ src/App.jsx
@@ -1,11 +1,17 @@
 import { useState } from 'react';
+import MonNav from './Composants/nav/Nav';
+import MonSousTitres from './Composants/sous-titres/SousTitres';
+import MonContact from './Composants/contact/Contact';
 import MonFooter from './Composants/footer/Footer';
 
 function App() {
-  const [count, setCount] = useState(0)
+  const [count, setCount] = useState(0);
 
   return (
     <>
+      <MonNav/>
+      <MonSousTitres/>
+      <MonContact/>
       <MonFooter/>
     </>
   );
