83d57ebe9e21e34ba82b7986d80916fb80561712
Chehrazade Bouzid <chehrazadebou@yahoo.fr>
2025-04-28 13:18:19 +0200

    Finalisation des modifications Card avant changement de branche

--- src/App.jsx
+++ src/App.jsx
@@ -1,35 +1,210 @@
-import { useState } from 'react'
-import reactLogo from './assets/react.svg'
-import viteLogo from '/vite.svg'
-import './App.css'
+import { useState } from 'react';
+import Card from './components/card/Card.jsx';
+import Filtre from './components/filtre/Filtre.jsx';
+import Compteur from './components/calcul/Compteur.jsx';
+
+// Tableau complet des recettes avec toutes les images
+const recettes = [
+  { 
+    titre: "Bowl Mangue",
+    image: "/image/bowlMangue.jpeg",
+    description: "Un délicieux bowl de mangue",
+    titre1: "Ingrédients :",
+    ingredients: [
+      { nom: "Courgette", quantite: 1},
+      { nom: "Pomme", quantite: 2},
+      { nom: "Poire", quantite: 3},
+      { nom: "Banane", quantite: 2}
+      
+    ],
+    couleur: "#9DCC9D",
+    personnes:4,
+    
+  },
+  {
+    titre: "Bowl Concombre",
+    image: "/image/bowlConcombre.jpeg",
+    description: "Un délicieux bowl de concombre",
+    titre1: "Ingrédients :",
+    ingredients: [
+      { nom: "Mangue", quantite: 1},
+      { nom: "Concombre", quantite: 2},
+      { nom: "Poire", quantite: 3},
+      { nom: "Cerise", quantite: 2}
+      
+    ],
+    couleur: "#cbaf96",
+    personnes:4,
+    
+  },
+  {
+    titre: "Salade Été",
+    image: "/image/saladeEte.jpeg",
+    description: "Salade fraîche d'été",
+    titre1: "Ingrédients :",
+    ingredients: [
+      { nom: "lait", quantite: 1},
+      { nom: "Pomme", quantite: 2},
+      { nom: "Poire", quantite: 3},
+      { nom: "Banane", quantite: 2}
+      
+    ],
+    couleur: "#bdb8f0",
+    personnes:4,
+    
+  },
+  {
+    titre: "Salade Hiver",
+    image: "/image/saladeHiver.jpeg",
+    description: "Salade hivernale riche",
+    titre1: "Ingrédients :",
+    ingredients: [
+      { nom: "Mangue", quantite: 1},
+      { nom: "Pomme", quantite: 2},
+      { nom: "Poire", quantite: 3},
+      { nom: "Banane", quantite: 2}
+      
+    ],
+    couleur: "#225542",
+    personnes:4,
+    
+  },
+  {
+    titre: "Bowl Potage 1",
+    image: "/image/bowlPotage.jpeg",
+    description: "Bowl chaud façon potage",
+    titre1: "Ingrédients :",
+    ingredients: [
+      { nom: "Mangue", quantite: 1},
+      { nom: "Pomme", quantite: 2},
+      { nom: "Poire", quantite: 3},
+      { nom: "Banane", quantite: 2}
+      
+    ],
+    couleur: "#57caf1",
+    personnes:4,
+    
+  },
+  {
+    titre: "Bowl Mille",
+    image: "/image/bowlMille.jpeg",
+    description: "Bowl aux mille saveurs",
+    titre1: "Ingrédients :",
+    ingredients: [
+      { nom: "Mangue", quantite: 1},
+      { nom: "Pomme", quantite: 2},
+      { nom: "Poire", quantite: 3},
+      { nom: "Banane", quantite: 2}
+      
+    ],
+    couleur: "#fa46ee",
+    personnes:4,
+    
+  },
+  {
+    titre: "Bowl Potage 2",
+    image: "/image/bowlPotage.jpeg",
+    description: "Deuxième version du bowl potage",
+    titre1: "Ingrédients :",
+    ingredients: [
+      { nom: "Mangue", quantite: 1},
+      { nom: "Pomme", quantite: 2},
+      { nom: "Poire", quantite: 3},
+      { nom: "Banane", quantite: 2}
+      
+    ],
+    couleur: "#fd7f68",
+    personnes:4,
+    
+  },
+ 
+  {
+    titre: "Salade de Fruits",
+    image: "/image/saladeFruit.jpeg",
+    description: "Un mélange de fruits frais",
+    titre1: "Ingrédients :",
+    ingredients: [
+      { nom: "Mangue", quantite: 1},
+      { nom: "Pomme", quantite: 2},
+      { nom: "Poire", quantite: 3},
+      { nom: "Banane", quantite: 2}
+      
+    ],
+    couleur: "#eae10e",
+    personnes:4,
+    
+  },
+  {
+    titre: "Tropic Bowl",
+    image: "/image/tropic.jpeg",
+    description: "Bowl tropical sucré",
+    titre1: "Ingrédients :",
+    ingredients: [
+      { nom: "Mangue", quantite: 1},
+      { nom: "Pomme", quantite: 2},
+      { nom: "Poire", quantite: 3},
+      { nom: "Banane", quantite: 2}
+      
+    ],
+    couleur: "#0eea95",
+    personnes:4,
+    
+  },
+  {
+    titre: "Délice Fruit 1",
+    image: "/image/deliceFruit.jpeg",
+    description: "Délice sucré aux fruits 1",
+    titre1: "Ingrédients :",
+    ingredients: [
+      { nom: "Mangue", quantite: 1},
+      { nom: "Pomme", quantite: 2},
+      { nom: "Poire", quantite: 3},
+      { nom: "Banane", quantite: 2}
+      
+    ],
+    couleur: "#1e1f3e",
+    personnes:4,
+    
+  },
+ 
+];
 
 function App() {
-  const [count, setCount] = useState(0)
+  const [filtre, setFiltre] = useState("");
+  const [nombrePersonnes, setNombrePersonnes] = useState(4);
+  
+  // Prend ce que l'utilisateur a écrit
+// Tout mettre en minuscules pour comparer facilement
+// Couper à chaque virgule pour avoir une liste
+// Enlever les espaces autour de chaque mot
+  const ingredientsExclus = filtre.toLowerCase().split(",").map(el => el.trim());
+  
+  // On garde seulement les recettes qui ne contiennent pas d'ingrédients interdits
+// Pour chaque ingrédient, on regarde s'il n'est PAS dans la liste des ingrédients exclus
+  const recettesFiltrees = recettes.filter(recette =>
+    recette.ingredients.every(ing =>
+      !ingredientsExclus.includes(ing.nom.toLowerCase())
+    )
+  );
 
   return (
-    <>
-      <div>
-        <a href="https://vite.dev" target="_blank">
-          <img src={viteLogo} className="logo" alt="Vite logo" />
-        </a>
-        <a href="https://react.dev" target="_blank">
-          <img src={reactLogo} className="logo react" alt="React logo" />
-        </a>
-      </div>
-      <h1>Vite + React</h1>
-      <div className="card">
-        <button onClick={() => setCount((count) => count + 1)}>
-          count is {count}
-        </button>
-        <p>
-          Edit <code>src/App.jsx</code> and save to test HMR
-        </p>
-      </div>
-      <p className="read-the-docs">
-        Click on the Vite and React logos to learn more
-      </p>
-    </>
-  )
+    <div className="page">
+      {/* Filtre */}
+      <Filtre filtre={filtre} setFiltre={setFiltre} />
+     
+
+      {/* Cartes */}
+      <main>
+        {recettesFiltrees.length > 0 ? (
+          recettesFiltrees.map((recette, i) => (
+            <Card key={i} {...recette} />
+          ))
+        ) : (
+          <p>Aucune recette trouvée</p>
+        )}
+      </main>
+    </div>
+  );
 }
 
-export default App
+export default App;
